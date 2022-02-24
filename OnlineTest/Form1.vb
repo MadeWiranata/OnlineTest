7845013bd0d4efa75bff661f950952887a9b390d
MadeWiranata <madeajhaa@gmail.com>
2022-02-24 16:11:00 +0700

    mst

---
+++ OnlineTest/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Dim kondisi As Boolean
+    Dim test1, test2 As String
+    Private Sub cek()
+        test1 = UCase(TextBox1.Text)
+        test2 = StrReverse(test1)
+        If test2.Equals(test1) Then
+            kondisi = True
+            MsgBox(kondisi)
+        Else
+            kondisi = False
+            MsgBox(kondisi)
+        End If
+    End Sub
+
+    Private Sub cek2()
+        Dim a, b, c, d, e, f, g, h, i As Single
+        a = Val(TextBox2.Text)
+        b = Val(TextBox3.Text)
+        c = Val(TextBox4.Text)
+        d = Val(TextBox5.Text)
+        e = Val(TextBox6.Text)
+        f = Val(TextBox7.Text)
+        g = Val(TextBox8.Text)
+        h = Val(TextBox9.Text)
+        i = Val(TextBox10.Text)
+
+        TextBox11.Text = a + e + i
+        TextBox12.Text = c + e + g
+        TextBox13.Text = a + e + i + c + e + g
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        cek2()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cek()
+    End Sub
+End Class
---
+++ OnlineTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("OnlineTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("OnlineTest")>
+<Assembly: AssemblyCopyright("Copyright © HP 2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("860fa56f-9f1d-4049-8f46-bb78fc24938e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
